90e35318aee0f7347c673bbf44dbb3c6f34976d8
koya-ken <koya.smp@gmail.com>
2020-06-14 14:10:07 +0900

    add test composite function differential

--- src/tests/test_core.py
+++ src/tests/test_core.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from core.features import numerical_diff
 from core import Variable
-from core.functions import Square
+from core.functions import Square, Exp
 import numpy as np
 
 
@@ -10,5 +10,16 @@ class TestCore(TestCase):
     def test_differential(self):
         f = Square()
         x = Variable(np.array(2.0))
+        dy = numerical_diff(f, x)
+        print(dy)
+
+    def test_composite_function_differential(self):
+        def f(x):
+            A = Square()
+            B = Exp()
+            C = Square()
+            return C(B(A(x)))
+
+        x = Variable(np.array(0.5))
         dy = numerical_diff(f, x)
         print(dy)
